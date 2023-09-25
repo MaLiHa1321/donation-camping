1b9fba7e7b79f41c1fd3fb4e28ccf5c14707a4ec
MaLiHa1321 <malihasultana2001@gmail.com>
2023-09-25 14:39:36 +0600

    statistics page functionality  are added

--- src/components/DonerDetails/DonerDetails.jsx
+++ src/components/DonerDetails/DonerDetails.jsx
@@ -5,13 +5,13 @@ const DonerDetails = ({doner}) => {
     console.log(doner)
     const {card_bg,picture,title,category,category_bg,text_button_bg,description,id,price} = doner || {};
     return (
-        <div className="flex gap-4" style={{backgroundColor: card_bg}} >
+        <div className="flex flex-col lg:flex-row gap-4" style={{backgroundColor: card_bg}} >
             <img src={picture} alt="" className="w-[200px] h-[200px]" />
             <div >
                 <h3 className="text-2xl rounded-lg mt-3" style={{backgroundColor: category_bg}}>{category}</h3>
             <h2 className="text-3xl">{title}</h2>
             <p style={{color: text_button_bg}}>Price: {price}</p>
-            
+
             <Link to={`/cate/${id}`}>
             <button className="btn" style={{backgroundColor: category_bg}}>Veiw Details</button>
             </Link>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,51 @@
-
+import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { PieChart, Pie } from 'recharts';
 
 const Statistics = () => {
+    const [donar, setDonar] = useState([])
+    const [total ,setTotal] = useState(0)
+    const [donTotal, setDontotal] = useState(0)
+    const donation = useLoaderData();
+
+    useEffect(()=>{
+        const donateItem = JSON.parse(localStorage.getItem('donate'))
+
+        const donationPrices = donation.map((card) => card.price);
+        const totalCardDonation = donationPrices.reduce((accumulator, price) => accumulator + price, 0);
+        const percentTotalDonation = (totalCardDonation ) /100;
+
+        setDontotal(percentTotalDonation)
+        if(donateItem){
+            const totalAmount = donateItem.reduce((accumulator, item) => accumulator + item.price, 0);
+            const percentTotalAmount = (totalAmount) /100;
+            setTotal(percentTotalAmount)
+
+        setDonar(donateItem)
+     }
+    //  console.log(donation)
+    },[])
+    const data = [
+        { name: 'Your Donation', value: total },
+        // { name: 'Remaining Donation', value: 100 - total },
+    ];
+
+    const donTotalData = [
+        { name: 'Total Donation', value: donTotal },
+        // { name: 'Remaining Donation', value: 100 - donTotal },
+    ]
+   
     return (
         <div>
             <h1>This is statistics section</h1>
+
+<PieChart width={730} height={250} >
+  <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#FF0000" />
+  <Pie data={donTotalData} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+</PieChart>
             
+            <p> your donation: {total}% </p>
+            <p>Total : {donTotal} %</p>
         </div>
     );
 };
